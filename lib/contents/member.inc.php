a85ac5cf260cc65851396c53ddf74533ad9684f0
NazirArifin <ceylon.rizan@gmail.com>
2023-08-18 09:42:13 +0700

    login simat switch to plugin

--- lib/contents/member.inc.php
+++ lib/contents/member.inc.php
@@ -27,7 +27,6 @@ use SLiMS\DB;
 use SLiMS\Json;
 use SLiMS\Captcha\Factory as Captcha;
 use Volnix\CSRF\CSRF;
-use GuzzleHttp\Client;
 use SLiMS\Plugins;
 
 // be sure that this file not accessed directly
@@ -106,116 +105,7 @@ if (isset($_POST['logMeIn']) && !$is_member_login) {
     // regenerate session ID to prevent session hijacking
     session_regenerate_id(true);
 
-    /**
-     * Edited on: 2019-04-01
-     * Purpose: Check user in SIMAT UNIRA. If user does not exist, then create new user.
-     *          If user exist, then update user data.
-     */ 
-    $client = new Client([
-      'base_uri' => 'https://api.unira.ac.id',
-      'timeout'  => 2.0,
-      'verify' => false,
-    ]);
-
-    try {
-      $response = $client->post('/v1/token', [
-        'headers' => [
-          'Content-Type' => 'application/x-www-form-urlencoded',
-        ],
-        'form_params' => [
-          'username' => $username,
-          'password' => $password,
-          'client' => 'simbio'
-        ],
-      ]);
-    } catch(\GuzzleHttp\Exception\RequestException $e) {
-      utility::writeLogs($dbs, 'member', $username, 'Login', sprintf(__('Login FAILED for member %s from address %s'),$username,ip()));
-      redirect()->withMessage('wrong_password', __('Login FAILED! Wrong Member ID or password!'))->back();
-    }
-
-    $json = json_decode($response->getBody()->getContents(), true);
-    $tokenAccess = $json['data']['attributes']['access'];
-    $tokenRefresh = $json['data']['attributes']['refresh'];
-
-    // get /v1/saya
-    $response = $client->get('/v1/saya', [
-      'headers' => [
-        'Authorization' => 'Bearer '.$tokenAccess,
-      ]
-    ]);
-
-    $json = json_decode($response->getBody()->getContents(), true);
-    $idUser = $json['data']['id'];
-    $type = $json['data']['attributes']['type'];
-
     Plugins::getInstance()->execute(Plugins::MEMBERSHIP_INIT);
-    if (! class_exists('simbio_date')) {
-      require SIMBIO.'simbio_UTILS/simbio_date.inc.php';
-    }
-
-    $result = $dbs->query("SELECT * FROM member WHERE member_id = '$idUser'");
-    $row = $result->fetch_assoc();
-
-    $data['member_id'] = $idUser;
-    $data['member_name'] = $json['data']['attributes']['nama'];
-    $data['inst_name'] = 'Universitas Madura';
-    $data['birth_date'] = implode('-', array_reverse(explode('/', $json['data']['attributes']['lahirtanggal'])));
-    $data['register_date'] = date('Y-m-d');
-    $data['expire_date'] = simbio_date::getNextDate(5000, $data['register_date']);
-    $data['member_since_date'] = $data['register_date'];
-    $data['pin'] = $password;
-    $data['member_address'] = $json['data']['attributes']['alamat'];
-    $data['member_mail_address'] = $json['data']['attributes']['email'];
-    $data['member_phone'] = $json['data']['attributes']['telepon'];
-    $data['member_fax'] = '';
-    $data['postal_code'] = '';
-    $data['member_notes'] = '';
-    $data['member_email'] = $json['data']['attributes']['email'];
-    $data['is_pending'] = 0;
-    $data['input_date'] = date('Y-m-d');
-    $data['last_update'] = date('Y-m-d');
-    $data['mpasswd'] = password_hash($password, PASSWORD_BCRYPT);
-
-    // jika $type == 'mhs', maka kita cek di tabel member
-    if ($type == 'mhs') {
-      // attributes['status'] harus 'aktif' / 'lulus' / 'bss'
-      $allowedStatus = ['aktif', 'lulus', 'bss'];
-      if (!in_array($json['data']['attributes']['status'], $allowedStatus)) {
-        utility::writeLogs($dbs, 'member', $username, 'Login', sprintf(__('Login FAILED for member %s from address %s'),$username,ip()));
-        redirect()->withMessage('wrong_password', __('Login FAILED! Wrong Member ID or password!'))->back();
-      }
-
-      $data['member_type_id'] = 1;
-      $data['gender'] = $json['data']['attributes']['jenisKelamin'] == 'L' ? 1 : 0;
-    }
-    // jika $type == 'dkr', maka kita cek di tabel member
-    if ($type == 'dkr') {
-      $data['member_type_id'] = 2;
-      $data['gender'] = 1;
-    }
-
-    // create sql op object
-    $sql_op = new simbio_dbop($dbs);
-
-    if ($row) {
-      // update data member if any
-      unset($data['input_date']);
-      Plugins::getInstance()->execute(Plugins::MEMBERSHIP_BEFORE_UPDATE, ['data' => $data]);
-
-      // update the data
-      $update = $sql_op->update('member', $data, "member_id = '$idUser'");
-      Plugins::getInstance()->execute(Plugins::MEMBERSHIP_AFTER_UPDATE, ['data' => api::member_load($dbs, $data['member_id'])]);
-    } else {
-      // download image and then save to
-      $filename = 'images/persons/member_' . $idUser . '.jpg';
-      $client->get('https://api.unira.ac.id/' . $json['data']['attributes']['thumbnail'], ['sink' => $filename]);
-      if (file_exists($filename)) {
-        $data['member_image'] = basename($filename);
-      }
-
-      $insert = $sql_op->insert('member', $data);
-      Plugins::getInstance()->execute(Plugins::MEMBERSHIP_AFTER_SAVE, ['data' => api::member_load($dbs, $data['member_id'])]);
-    }
 
     // create logon class instance
     $logon = new member_logon($username, $password, $sysconf['auth']['member']['method']);
