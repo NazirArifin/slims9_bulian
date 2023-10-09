51b000f1735583a3b2d69e9384d959af96ca0fe8
NazirArifin <ceylon.rizan@gmail.com>
2023-10-09 12:05:35 +0700

    perbaiki pagination di plugin

--- plugins/bebas_pustaka/index.php
+++ plugins/bebas_pustaka/index.php
@@ -15,10 +15,13 @@ require LIB . 'ip_based_access.inc.php';
 do_checkIP('smc');
 do_checkIP('smc-bibliography');
 // start session
-require SB . 'admin/default/session.inc.php';
-require SIMBIO . 'simbio_GUI/table/simbio_table.inc.php';
-require SIMBIO . 'simbio_GUI/form_maker/simbio_form_table_AJAX.inc.php';
-require SIMBIO . 'simbio_DB/datagrid/simbio_dbgrid.inc.php';
+require SB.'admin/default/session.inc.php';
+require SB.'admin/default/session_check.inc.php';
+require SIMBIO.'simbio_GUI/table/simbio_table.inc.php';
+require SIMBIO.'simbio_GUI/form_maker/simbio_form_table_AJAX.inc.php';
+require SIMBIO.'simbio_GUI/paging/simbio_paging.inc.php';
+require SIMBIO.'simbio_DB/datagrid/simbio_dbgrid.inc.php';
+require SIMBIO.'simbio_DB/simbio_dbop.inc.php';
 
 // privilege check
 $can_read = utility::havePrivilege('membership', 'r');
